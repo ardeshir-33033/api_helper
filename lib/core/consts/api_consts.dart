3ca404540a8ddedc60271a785791ca55301f9e91
ardeshir <ardeshirojan2@gmail.com>
2024-07-14 18:26:38 +0330

    update token for pickopack

--- lib/core/consts/api_consts.dart
+++ lib/core/consts/api_consts.dart
@@ -7,17 +7,17 @@ import 'language.dart';
 
 class ApiConstants {
   Map<String, String> imageHeader = {
-    HttpHeaders.authorizationHeader: "Bearer ${Token().token ?? ""}",
+    HttpHeaders.authorizationHeader: "${Token().token ?? ""}",
     "User-Agent": UserAgent().userAgent ?? '',
-    "Content-Language" : Language().language ?? 'en',
+    "Content-Language": Language().language ?? 'en',
     // "Accept": "multipart/byteranges",
     // "content-type": "image/jpeg; charset=utf-8",
   };
 
   Map<String, String> bearerHeader = {
-    HttpHeaders.authorizationHeader: "Bearer ${Token().token ?? ""}",
+    HttpHeaders.authorizationHeader: "${Token().token ?? ""}",
     "User-Agent": UserAgent().userAgent ?? '',
-    "Content-Language" : Language().language ?? 'en',
+    "Content-Language": Language().language ?? 'en',
 
     // "Accept": "application/json",
     // "content-type": "application/json; charset=utf-8",
@@ -25,14 +25,14 @@ class ApiConstants {
 
   Map<String, String> formDataHeader = {
     "User-Agent": UserAgent().userAgent ?? '',
-    "Content-Language" : Language().language ?? 'en',
+    "Content-Language": Language().language ?? 'en',
     // "Accept": "multipart/form-data",
     // "content-type": "application/json; charset=utf-8",
   };
 
   Map<String, String> basicHeader = {
     "User-Agent": UserAgent().userAgent ?? '',
-    "Content-Language" : Language().language ?? 'en',
+    "Content-Language": Language().language ?? 'en',
     // "Accept": "application/json",
     // "content-type": "application/json; charset=utf-8",
   };
